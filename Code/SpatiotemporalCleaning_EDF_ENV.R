654786a865048100ef933e152c2a9e02c9354389
federicoandrade <federicoandrade@gmail.com>
2020-12-15 22:14:18 -0800

    Falta revisar consistencia. Pero todo pega

--- Code/SpatiotemporalCleaning_EDF_ENV.R
+++ Code/SpatiotemporalCleaning_EDF_ENV.R
@@ -85,10 +85,74 @@ edf2013 <- edf2013 %>%
   unite("fecha_insc", anio_insc, mes_insc, dia_insc, sep = "-", remove = FALSE) %>% 
   unite("fecha_fall", anio_fall, mes_fall, dia_fall, sep = "-", remove =FALSE) %>% 
   unite("fecha_mad", anio_mad, mes_mad, dia_mad, sep = "-", remove = FALSE) %>% 
-  select(-cod_esta, -cod_pais)
+  select(-cod_esta, -cod_pais, -ofi_insc, -folio, -acta_insc)
    
 edf2013$fecha_insc <- as.Date(edf2013$fecha_insc)
 edf2013$fecha_fall <- as.Date(edf2013$fecha_fall)
 edf2013$fecha_mad <- as.character(edf2013$fecha_mad)
 
 edf2013_2019 <- bind_rows(edf2013, edf2014_2019)
+
+# preparing 2012
+#No hay dato del día  para fecha_insc, y para fecha_fall (le puse 15 a todos =)!
+#Se pierden variables de fecha nacimiento madre. Creo que no importa porque está la edad. 
+#NOTE 2012 and 2011 fech_insc and fecha_fall DAY is not accurate, all set to 15
+
+edf2012 <- edf2012 %>% 
+  mutate(col15 = 15) %>% 
+  unite("fecha_insc", anio_insc, mes_insc, col15, sep = "-", remove = FALSE) %>% 
+  unite("fecha_fall", anio_def, mes_def, col15, sep = "-", remove =FALSE) %>%
+  rename(hij_viv = hij_vivos, anio_fall = anio_def, mes_fall = mes_def, causa_fetal = cau_10ar) %>% 
+  select(-cod_pais, -ofi_insc, -acta_insc, -col15)
+
+edf2012$fecha_insc <- as.Date(edf2012$fecha_insc)
+edf2012$fecha_fall <- as.Date(edf2012$fecha_fall)
+
+edf2012_2019 <- bind_rows(edf2012, edf2013_2019)
+#SUma es consistente
+
+#preparing 2011
+
+edf2011 <- edf2011 %>% 
+  within(lug_fall[lug_fall == "01"] <- "1") %>% 
+  within(lug_fall[lug_fall == "02"] <- "2") %>% 
+  within(lug_fall[lug_fall == "03"] <- "4") %>% 
+  within(lug_fall[lug_fall == "04"] <- "5") %>% 
+  within(lug_fall[lug_fall == "05"] <- "6") %>% 
+  within(lug_fall[lug_fall == "06"] <- "7") %>% 
+  within(sexo[sexo == "01"] <- "1") %>% 
+  within(sexo[sexo == "02"] <- "2") %>% 
+  within(sabe_leer[sabe_leer == "01"] <- "1") %>% 
+  within(sabe_leer[sabe_leer == "02"] <- "2") %>% 
+  within(sabe_leer[sabe_leer == "09"] <- "9")
+
+edf2011$lug_fall <- as.numeric(as.character(edf2011$lug_fall))
+edf2011$sexo <- as.numeric(as.character(edf2011$sexo))
+
+edf2011 <- edf2011 %>% 
+  select(-sem_gest)
+
+edf2011 <- edf2011 %>% 
+  mutate(col15 = 15) %>% 
+  unite("fecha_insc", anio_insc, mes_insc, col15, sep = "-", remove = FALSE) %>% 
+  unite("fecha_fall", anio_fall, mes_fall, col15, sep = "-", remove =FALSE) %>%
+  rename(hij_viv = hij_vivos, sem_gest = sem_gest1, causa_fetal = causa, asis_por = asit_por1, lugar_ocur = lug_fall) %>%
+  select(-vcausa, -aten_medica, -asit_por, -t, -th, -tm, -act_insc, -col15)
+
+edf2011$fecha_insc <- as.Date(edf2011$fecha_insc)
+edf2011$fecha_fall <- as.Date(edf2011$fecha_fall)
+
+
+edf2011$anio_insc <- as.numeric(edf2011$anio_insc)
+edf2011$mes_insc <- as.numeric(as.character(edf2011$mes_insc))
+edf2011$anio_fall <- as.numeric(as.character(edf2011$anio_fall))
+edf2011$mes_fall <- as.numeric(as.character(edf2011$mes_fall))
+edf2011$p_emb <- as.numeric(as.character(edf2011$p_emb))
+edf2011$area_fall <- as.numeric(as.character(edf2011$area_fall))
+edf2011$sabe_leer <- as.numeric(as.character(edf2011$sabe_leer))
+edf2011$niv_inst <- as.numeric(as.character(edf2011$niv_inst))
+edf2011$area_res <- as.numeric(as.character(edf2011$area_res))
+edf2011$est_civil <- as.numeric(as.character(edf2011$est_civil))
+edf2011$sem_gest <- as.numeric(as.character(edf2011$sem_gest))
+
+edf2011_2019 <- bind_rows(edf2011, edf2012_2019)
